fcdace4677948ee565f05b5dad8ec21943f0a2a4
FlorianDialer <florian.dialer@stud-mail.uni-wuerzburg.de>
2025-04-12 15:14:45 +0200

    updating comments and function structure

--- R/S2_data_download.R
+++ R/S2_data_download.R
@@ -13,7 +13,7 @@
 
 S2_data_download <- function(username, password, start_date, end_date, aoi, condition, cloud_cover_percent = 50, number_of_results = 12) {
 
-  # Retrieve Access Token from Copernicus Hub for later download
+  #Retrieve Access Token from Copernicus Hub for later download
   access_token_retrival <- list(client_id = "cdse-public",
                                 username = username,
                                 password = password,
@@ -37,7 +37,8 @@ S2_data_download <- function(username, password, start_date, end_date, aoi, cond
     message("A high value of number_if_results may fail depending on API load.")
   }
 
-  # From aoi.shp/gpkg create Bounding Box and convert to WKT format for the query
+
+  #From aoi.shp/gpkg create Bounding Box and convert to WKT format for the query
   if(tools::file_ext(aoi) != "shp" & tools::file_ext(aoi) != "gpkg") stop("The provided AOI file needs to be in format .shp or .gpkg!")
 
   aoi <- sf::st_read(aoi, quiet = TRUE)
@@ -45,17 +46,19 @@ S2_data_download <- function(username, password, start_date, end_date, aoi, cond
 
   bbox <- sf::st_as_sfc(sf::st_bbox(aoi[1,])) # always the first feature gets selected!
 
-  # Reformatting because of API request format deviations
+
+  #Reformatting because of API request format deviations
   bbox_WKT <- sf::st_as_text(bbox)
   bbox_WKT_formatted <- gsub("POLYGON ", "POLYGON", bbox_WKT)
   bbox_WKT_formatted <- paste0(bbox_WKT_formatted, "'")
 
 
-  # Defining missing attributes for the query for retrieving Sentinel-2 BOA data
+  #Defining missing attributes for the query for retrieving Sentinel-2 BOA data
   data_collection <- "SENTINEL-2"
   product_type <- "S2MSI2A"
 
-  # Creating request string for getting query results
+
+  #Creating request string for getting query results
   request_string <- glue::glue("https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=Collection/Name eq '{data_collection}' and OData.CSC.Intersects(area=geography'SRID=4326;{bbox_WKT_formatted}) and Attributes/OData.CSC.DoubleAttribute/any(att:att/Name eq 'cloudCover' and att/OData.CSC.DoubleAttribute/Value le {cloud_cover_percent}.00) and Attributes/OData.CSC.StringAttribute/any(att:att/Name eq 'productType' and att/OData.CSC.StringAttribute/Value eq '{product_type}') and ContentDate/Start gt {start_date}T00:00:00.000Z and ContentDate/Start lt {end_date}T00:00:00.000Z&$top={number_of_results}&$orderby=PublicationDate asc")
   request_string_no_spaces <- toString(gsub(" ", "%20", request_string))
 
@@ -63,19 +66,11 @@ S2_data_download <- function(username, password, start_date, end_date, aoi, cond
   granule_json_data_content <- httr::content(granule_json_data, "parsed")
 
 
-
-  #create temp folder if it doesn't exist yet for upcoming operations
-  temp_directory <- file.path(getwd(), "temp")
-
-  if (!dir.exists(temp_directory)) {
-    dir.create(temp_directory)
-  }
-
-  # Let user choose tiles based on result
+  #Let user choose tiles based on result
   granule_IDs <- sapply(X = granule_json_data_content$value, function(X) X$Id)
 
-  #check if there is valid data
 
+  #Check if there is valid data
   if(length(granule_IDs)==0) stop("There are no Sentinel-2 tiles with your selected options! Try to change/check dates, cloud_cover_percentage etc. or run the function again.")
 
   footprints <- sapply(X = granule_json_data_content$value, function(X) X$Footprint)
@@ -83,7 +78,16 @@ S2_data_download <- function(username, password, start_date, end_date, aoi, cond
   time_stamps <- sapply(X = granule_json_data_content$value, function(X) X$ContentDate$Start)
   time_stamps_formatted <- unname(sapply(time_stamps, function(X) substr(X, 1, 10)))
 
-  # loop to extract the data extent of each granule for user to chose intersecting tiles to cover AOI fully
+
+  #Create temp folder for tile_footprint shapes
+  temp_directory <- file.path(getwd(), "temp")
+
+  if (!dir.exists(temp_directory)) {
+    dir.create(temp_directory)
+  }
+
+
+  #Loop to extract the data extent of each granule for user to chose intersecting tiles to cover AOI fully
   for (footprint_ID in seq_along(footprints)) {
     footprints_conversion_1 <- gsub("geography'", "", footprints[footprint_ID])
     footprints_conversion_2 <- gsub("'", "", footprints_conversion_1)
@@ -96,8 +100,7 @@ S2_data_download <- function(username, password, start_date, end_date, aoi, cond
   }
 
 
-  #creating function selecting tiles
-
+  #Creating function for selecting tiles
   tile_footprint_list <- list.files(temp_directory, pattern = "\\.gpkg$", full.names = TRUE)
 
   tile_footprint_sf <- sapply(tile_footprint_list, sf::st_read, quiet = TRUE)
@@ -115,15 +118,16 @@ S2_data_download <- function(username, password, start_date, end_date, aoi, cond
       stop("There was an error plotting your AOI, make sure that only you desired shape is in your shp./gpkg. file containing the geometry.")
     }
   }
+
   unlink(temp_directory, recursive=TRUE)
   backup_granule_IDs <- granule_IDs
 
 
+  #Get user answer and extract the correct granules
   tile_answer <- readline(prompt = "Select S2 tile(s) for your AOI (e.g. 1 or 3,4,6):")
   tile_answer <- unlist(strsplit((tile_answer), split = ","))
 
   grDevices::graphics.off()
-
 
   selected_granules <- vector()
 
@@ -137,21 +141,18 @@ S2_data_download <- function(username, password, start_date, end_date, aoi, cond
       }
     }
 
-  # selected_granules <- stats::na.omit(selected_granules)
   selected_granules <- as.vector(selected_granules)
 
-  headers <- httr::add_headers(Authorization = glue::glue("Bearer {access_token}"))
 
-
-  #create data folder if it doesn't exist yet
+  #Create data folder if it doesn't exist yet
   temp_data_directory <- file.path(getwd(), "temp_data_directory")
 
   if (!dir.exists(temp_data_directory)) {
     dir.create(temp_data_directory)
   }
 
-  #create condition subdirectory
 
+  #Create condition subdirectory
   temp_data_condition_directory <- file.path(temp_data_directory, condition)
 
   if (!dir.exists(temp_data_condition_directory)) {
@@ -159,12 +160,17 @@ S2_data_download <- function(username, password, start_date, end_date, aoi, cond
   }
 
 
-  # get bands based on user choosing AOI
+  #Create header with access_token to authenticate for download via API
+  headers <- httr::add_headers(Authorization = glue::glue("Bearer {access_token}"))
+
+
+  #Get individual bands based on user choosing tiles
   for (granule_ID in selected_granules) {
 
     message(paste0("Downloading selected tile ", which(selected_granules==granule_ID), " of ", length(selected_granules), "..." ))
 
 
+    #Iteratively build strings for finding the correct link/path for downloading individual bands
     nodes_base_url <- glue::glue("https://download.dataspace.copernicus.eu/odata/v1/Products({granule_ID})/Nodes")
 
     nodes_1 <- httr::GET(url = nodes_base_url, headers)
@@ -173,10 +179,8 @@ S2_data_download <- function(username, password, start_date, end_date, aoi, cond
     nodes_2 <- httr::GET(glue::glue("{nodes_base_url}({nodes_1_content})/Nodes(GRANULE)/Nodes"))
     nodes_2_content <- httr::content(nodes_2)$result[[1]]$Name[1]
 
-    #nodes_3 <- httr::GET(glue::glue("{nodes_base_url}({nodes_1_content})/Nodes(GRANULE)/Nodes({nodes_2_content})/Nodes"))
     nodes_3_content <- "IMG_DATA"
 
-    #nodes_4 <- httr::GET(glue::glue("{nodes_base_url}({nodes_1_content})/Nodes(GRANULE)/Nodes({nodes_2_content})/Nodes({nodes_3_content})/Nodes"))
     nodes_4_content <- "R10m"
 
     nodes_5 <- httr::GET(glue::glue("{nodes_base_url}({nodes_1_content})/Nodes(GRANULE)/Nodes({nodes_2_content})/Nodes({nodes_3_content})/Nodes({nodes_4_content})/Nodes"))
@@ -194,12 +198,15 @@ S2_data_download <- function(username, password, start_date, end_date, aoi, cond
     nodes_6_band_names <- sapply(nodes_6_content, function(X) X$Name)
     nodes_6_content_SCL <- nodes_6_band_names[grepl("_SCL_", nodes_6_band_names)]
 
+
+    #Directory for downloaded data
     granule_directory <- file.path(temp_data_condition_directory, granule_ID)
 
     if (!dir.exists(granule_directory)) {
       dir.create(granule_directory)
     }
 
+    #Getting individual files based on created/found links/paths
     meta_data <- httr::GET(url = glue::glue("https://download.dataspace.copernicus.eu/odata/v1/Products({granule_ID})/Nodes({nodes_1_content})/Nodes(MTD_MSIL2A.xml)/$value"), headers, httr::write_disk(paste0(granule_directory, glue::glue("/{granule_ID}_meta_data.xml")), overwrite = T))
     B02 <- httr::GET(url = glue::glue("https://download.dataspace.copernicus.eu/odata/v1/Products({granule_ID})/Nodes({nodes_1_content})/Nodes(GRANULE)/Nodes({nodes_2_content})/Nodes(IMG_DATA)/Nodes(R10m)/Nodes({nodes_5_content_B02})/$value"), headers, httr::write_disk(paste0(granule_directory, glue::glue("/{granule_ID}_B02.jp2")), overwrite = T))
     B03 <- httr::GET(url = glue::glue("https://download.dataspace.copernicus.eu/odata/v1/Products({granule_ID})/Nodes({nodes_1_content})/Nodes(GRANULE)/Nodes({nodes_2_content})/Nodes(IMG_DATA)/Nodes(R10m)/Nodes({nodes_5_content_B03})/$value"), headers, httr::write_disk(paste0(granule_directory, glue::glue("/{granule_ID}_B03.jp2")), overwrite = T))
